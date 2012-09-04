dddfd517bcd7e42cf898cbb89c6d015cc72b1cda
Michelle Thomas <michelle.q.thomas@gmail.com>
2012-09-04 09:29:47 -0700

    Changes to the singly linked list

--- Makefile
+++ Makefile
@@ -2,6 +2,11 @@ CFLAGS=-I/Users/michelle/play/see_play/
 stack: rpn_calculator.o
 	gcc -o stack rpn_calculator.o
 
+list: my_list.o
+	gcc -o list my_list.o
+
 rpn_calculator.o: rpn_calculator.c stack.h
 	gcc -c $(CFLAGS) rpn_calculator.c
 
+my_list.o: my_list.c list.h
+	gcc -c $(CFLAGS) my_list.c
--- list.h
+++ list.h
@@ -1,31 +1,87 @@
-struct list_node{
-    int val;
-    list_node* next;
+typedef struct list_node_1 * List_node;
+typedef struct list_1 * List;
+
+struct list_node_1{
+  int val;
+  List_node* next;
+};
+
+struct list_1{
+  List_node* head;
+  List_node* tail;
+};
+
+
+void push(int val, List list){
+  List_node retVal = malloc(sizeof(List_node));
+  retVal->val = val;
+  if (list->head == NULL){
+    list->head = retVal;
+    list->tail = retVal;
+    retVal->next = NULL;
+  }
+  else{
+    List_node* last = list->head;
+    list->head = retVal;
+    retVal->next = last;
+  }
+  List_node head = list->head;
+  printf("Added: %d \n", retVal->val);
+  printf("Front now: %d \n", head->val);
 }
 
-struct list{
-    list_node* head;
-    list_node* tail;
+void enqueue(int val, List list){
+  List_node retVal = malloc(sizeof(List_node));
+  retVal->val = val;
+  if (list->head == NULL){
+    list->head = retVal;
+    list->tail = retVal;
+    retVal->next = NULL;
+  }
+  else{
+    List_node last = list->tail;
+    list->tail = retVal;
+    last->next = list->tail;
+    retVal->next = NULL;
+  }
+  List_node tail = list->tail;
+  printf("Added: %d \n", retVal->val);
+  printf("Back now: %d \n", tail->val);
 }
 
-typedef struct list_node * list_node;
-typedef struct list * list;
+void dequeue(List list){
+  List_node remove = list->head;
+  list->head = remove->next;
+  free(remove);
+}
 
+void pop(List list){
+  List_node remove = list->head;
+  list->head = remove->next;
+  free(remove);
+}
 
-void add(int val, list* l){
-    list_node retVal = malloc(sizeof(list_node));
-    retVal->val = val;
-    if (l->head == NULL){
-        l->head = retVal;
-        l->tail = retVal;
-        retVal->next = NULL;
+List new_list(){
+  List retVal = malloc(sizeof(List));
+  return retVal;
+}
+
+void traverse_list(List list){
+  int i = 0;
+  List_node head = list->head;
+  if(head==NULL){
+    printf("There is nothing in this list %d \n", i);
+  }
+  else if(head->next == NULL){
+    printf("Item: %d", head->val);
+  }
+  else{
+    List_node current = head;
+    while(current!=NULL){
+      printf("%d\n", current->val);
+      current = current->next;
     }
-    else{
-        list_node* last;
-        last = l->head;
-        l->head = retVal;
-        retVal->next = last;
-    }
+  }
 }
 
 
