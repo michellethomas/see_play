aa6e2a6aa1f27a683aa991a9944bf1e3bc5a9a6d
Michelle Thomas <michelle.q.thomas@gmail.com>
2012-09-11 00:41:17 -0700

    Final changes to allow instructions to be taken from a file at the command line

--- my_list.c
+++ my_list.c
@@ -2,32 +2,47 @@
 #include <stdlib.h>
 #include <list.h>
 #include <stdio.h>
+#include <string.h>
 
 void file_to_read(char* file_name, List list){
-  char line[20];
+  char buffer[256];
   int i = 0;
+  char* buffer_str;
+  int num = 0;
   FILE *file_to_read = fopen(file_name,"r");
   if (file_to_read == NULL){
     printf("You tried to open %s, which doesn't exist", file_name);
     exit(1);
   } 
-  while(fgets(line, 20, file_to_read) != NULL){
-    for(i=0; i<=sizeof(line); i++){
-      printf("%c", line[i]);
+  while(fgets(buffer, 256, file_to_read) != NULL){
+    buffer_str = strtok(buffer, " ");
+    if(strcmp(buffer_str, "push")==0){
+      buffer_str = strtok(NULL, " ");
+      num = atol(buffer_str);
+      push(num, list);
+    }
+    else if(strcmp(buffer_str, "enqueue")==0){
+      buffer_str = strtok(NULL, " ");
+      num = atol(buffer_str);
+      enqueue(num, list);
+    }
+    else if(strcmp(buffer_str, "pop")==0){
+      pop(list);
+    }
+    else if(strcmp(buffer_str, "dequeue")==0){
+      dequeue(list);
     }
   }
-  //fscanf(file_to_read, "%s %d", operation, value);
 }
 
 int main(int argc, char **argv){
-  List list;
+  List list = new_list();
   if (argv[1] == NULL){
     printf("You need to give the name of your file");
   }
   else{
     file_to_read(argv[1], list);
   }
-  list = new_list();
   push(1, list); 
   push(2, list);
   enqueue(3, list);
